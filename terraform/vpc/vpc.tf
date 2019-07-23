fd0bb7de50c2bcc1ba06dfda23bba46de5270d18
NewDev0501 <ds422186@gmail.com>
2019-07-23 21:04:18 +0530

    associate public ip vpc

--- terraform/vpc/vpc.tf
+++ terraform/vpc/vpc.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  profile    = "default"
-  region     = "${var.region}"
+  profile = "default"
+  region  = "${var.region}"
 }
 
 
@@ -24,7 +24,7 @@ resource "aws_internet_gateway" "demo_ig" {
 
 # Public subnet
 resource "aws_subnet" "vpc_public_sn" {
-  vpc_id = "${aws_vpc.vpc_name.id}"
+  vpc_id     = "${aws_vpc.vpc_name.id}"
   cidr_block = "${var.vpc_public_subnet_1_cidr}"
   //availability_zone = "${lookup(var.availability_zone, var.vpc_region)}"
   availability_zone = "us-east-1a"
@@ -35,8 +35,8 @@ resource "aws_subnet" "vpc_public_sn" {
 
 # Private subnet
 resource "aws_subnet" "vpc_private_sn" {
-  vpc_id = "${aws_vpc.vpc_name.id}"
-  cidr_block = "${var.vpc_private_subnet_1_cidr}"
+  vpc_id            = "${aws_vpc.vpc_name.id}"
+  cidr_block        = "${var.vpc_private_subnet_1_cidr}"
   availability_zone = "us-east-1b"
   /*tags {
     Name = "vpc_private_sn"
@@ -57,15 +57,16 @@ resource "aws_route_table" "vpc_public_sn_rt" {
 
 # Associate the routing table to public subnet
 resource "aws_route_table_association" "vpc_public_sn_rt_assn" {
-  subnet_id = "${aws_subnet.vpc_public_sn.id}"
+  subnet_id      = "${aws_subnet.vpc_public_sn.id}"
   route_table_id = "${aws_route_table.vpc_public_sn_rt.id}"
 }
 
 #Associate a new EC2 instance with VPC
 resource "aws_instance" "ec2" {
-  ami           = "ami-0b898040803850657"
-  instance_type = "t2.micro"
-  subnet_id = "${aws_subnet.vpc_public_sn.id}"
+  ami                         = "ami-0b898040803850657"
+  instance_type               = "t2.micro"
+  subnet_id                   = "${aws_subnet.vpc_public_sn.id}"
+  associate_public_ip_address = "true"
 }
 
 # ECS Instance Security group
