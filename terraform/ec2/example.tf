4f9a830115987c48aab821c45c24e9548e604b34
NewDev0501 <ds422186@gmail.com>
2019-07-23 19:46:06 +0530

    vpc-ec2-peering

--- terraform/ec2/example.tf
+++ terraform/ec2/example.tf
@@ -7,3 +7,7 @@ resource "aws_instance" "example" {
   ami           = "ami-0b898040803850657"
   instance_type = "t2.micro"
 }
+
+resource "aws_vpc_peering_connection" "vpc-0d04ce1ec29cb411a" {
+  vpc_id        = "vpc-0d04ce1ec29cb411a"
+}
