3b4d883e97bf7a00ce086a2019c733c2b2b4ee52
NewDev0501 <ds422186@gmail.com>
2019-07-23 22:06:56 +0530

    changing ami for ec2 instance for new region eu-north-1

--- terraform/vpc/vpc.tf
+++ terraform/vpc/vpc.tf
@@ -63,7 +63,8 @@ resource "aws_route_table_association" "vpc_public_sn_rt_assn" {
 
 #Associate a new EC2 instance with VPC
 resource "aws_instance" "ec2" {
-  ami                         = "ami-0b898040803850657"
+  # ami                         = "ami-0b898040803850657" #for us-east-1 region
+  ami                         = "ami-3f36be41" #for eu-north-1 region
   instance_type               = "t2.micro"
   subnet_id                   = "${aws_subnet.vpc_public_sn.id}"
   associate_public_ip_address = "true"
