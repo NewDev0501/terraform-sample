54db26a23176f4071da8153fccd929a5c1de80c0
NewDev0501 <ds422186@gmail.com>
2019-07-24 22:38:13 +0530

    instances fix

--- terraform/vpc/elb.tf
+++ terraform/vpc/elb.tf
@@ -1,45 +1,45 @@
 
 resource "aws_alb" "ecs-load-balancer" {
-    name                = "ecs-load-balancer"
-    security_groups     = ["${aws_security_group.vpc_public_sg.id}"]
-    subnets             = ["${aws_subnet.vpc_public_sn.id}", "${aws_subnet.vpc_private_sn.id}"]
+  name            = "ecs-load-balancer"
+  security_groups = ["${aws_security_group.vpc_public_sg.id}"]
+  subnets         = ["${aws_subnet.vpc_public_sn.id}", "${aws_subnet.vpc_private_sn.id}"]
 
-    tags = {
-      Name = "ecs-load-balancer"
-    }
+  tags = {
+    Name = "ecs-load-balancer"
+  }
+  instances = ["${aws_instance.ec2-01.id}", "${aws_instance.ec2-02.id}"]
 }
 
 resource "aws_alb_target_group" "ecs-target-group" {
-    name                = "ecs-target-group"
-    port                = "80"
+  name     = "ecs-target-group"
+  port     = "80"
+  protocol = "HTTP"
+  vpc_id   = "${aws_vpc.vpc_name.id}"
+
+  health_check {
+    healthy_threshold   = "5"
+    unhealthy_threshold = "2"
+    interval            = "30"
+    matcher             = "200"
+    path                = "/"
+    port                = "traffic-port"
     protocol            = "HTTP"
-    vpc_id              = "${aws_vpc.vpc_name.id}"
+    timeout             = "5"
+  }
 
-    health_check {
-        healthy_threshold   = "5"
-        unhealthy_threshold = "2"
-        interval            = "30"
-        matcher             = "200"
-        path                = "/"
-        port                = "traffic-port"
-        protocol            = "HTTP"
-        timeout             = "5"
-    }
+  tags = {
+    Name = "ecs-target-group"
+  }
 
-    tags = {
-      Name = "ecs-target-group"
-    }
-
-    instances = ["${aws_instance.ec2-01.id}","${aws_instance.ec2-02.id}"]
 }
 
 resource "aws_alb_listener" "alb-listener" {
-    load_balancer_arn = "${aws_alb.ecs-load-balancer.arn}"
-    port              = "80"
-    protocol          = "HTTP"
+  load_balancer_arn = "${aws_alb.ecs-load-balancer.arn}"
+  port              = "80"
+  protocol          = "HTTP"
 
-    default_action {
-        target_group_arn = "${aws_alb_target_group.ecs-target-group.arn}"
-        type             = "forward"
-    }
+  default_action {
+    target_group_arn = "${aws_alb_target_group.ecs-target-group.arn}"
+    type             = "forward"
+  }
 }
