846ddff35247c0fbf2903e6432442194666dd51a
NewDev0501 <ds422186@gmail.com>
2019-07-25 22:33:04 +0530

    security group fix

--- terraform/vpc/ec2.tf
+++ terraform/vpc/ec2.tf
@@ -5,6 +5,7 @@ resource "aws_instance" "ec2-01" {
   ami                         = "ami-4bd45f35"
   instance_type               = "t3.micro"
   subnet_id                   = "${aws_subnet.vpc_public_sn.id}"
+  vpc_security_group_ids      = ["${aws_security_group.vpc_public_sg.id}"]
   associate_public_ip_address = "true"
   user_data                   = "${file("userdata.sh")}"
 }
@@ -14,6 +15,7 @@ resource "aws_instance" "ec2-02" {
   ami                         = "ami-4bd45f35"
   instance_type               = "t3.micro"
   subnet_id                   = "${aws_subnet.vpc_public_sn.id}"
+  vpc_security_group_ids      = ["${aws_security_group.vpc_public_sg.id}"]
   associate_public_ip_address = "true"
   user_data                   = "${file("userdata.sh")}"
 }
